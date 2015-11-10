ba53517e2db005196ce54884b42cb611e7de34c6
Nur Rony <pro.nmrony@gmail.com>
2015-11-10 19:52:58 +0600

    immutable list test

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,8 +1,9 @@
 
 import {expect} from 'chai';
+import {List} from 'immutable'
 
 describe('immutability', () => {
-
+  //learning test
   describe('is a number', () => {
 
     function increment(currentState) {
@@ -17,4 +18,27 @@ describe('immutability', () => {
       expect(state).to.equal(42);
     });
   });
+
+  //List test
+  describe('A List', () => {
+    function AddMovie(currentState, movie) {
+      return currentState.push(movie);
+    }
+
+    it('is immutable', () => {
+      let state = List.of('Transporting', '28 Days Later');
+      let nextState = AddMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(List.of(
+        'Transporting',
+        '28 Days Later',
+        'Sunshine'
+      ));
+
+      expect(state).to.equal(List.of('Transporting', '28 Days Later'));
+    });
+  });
+
+  //Map test
+
 });
