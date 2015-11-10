6805e2ecbec107162f08803c6bc0604d09dda680
Nur Rony <pro.nmrony@gmail.com>
2015-11-10 21:12:53 +0600

    next for vote immutability test added

--- test/core_spec.js
+++ test/core_spec.js
@@ -23,6 +23,19 @@ describe('Application logic', ()=> {
 
   //test next function
   describe('next', () => {
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
+      }));
+    });
 
   });
+
 });
