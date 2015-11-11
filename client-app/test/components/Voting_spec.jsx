ea9379d7b8104d9855aaaf5186f38a5d4780d8a6
Nur Rony <pro.nmrony@gmail.com>
2015-11-11 19:58:56 +0600

    SOC for voting

--- client-app/test/components/Voting_spec.jsx
+++ client-app/test/components/Voting_spec.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react-addons-test-utils';
 import Voting from '../../src/components/Voting';
 import {expect} from 'chai';
@@ -29,4 +30,37 @@ describe('Voting', () => {
     expect(votedWith).to.equal('Trainspotting');
   });
 
+  it('disables buttons when user has voted', () => {
+    const component = ReactTestUtils.renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 Days Later"]}
+              hasVoted="Trainspotting" />
+    );
+    const buttons = ReactTestUtils.scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons.length).to.equal(2);
+    expect(buttons[0].hasAttribute('disabled')).to.equal(true);
+    expect(buttons[1].hasAttribute('disabled')).to.equal(true);
+  });
+
+  it('adds label to the voted entry', () => {
+    const component = ReactTestUtils.renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 Days Later"]}
+              hasVoted="Trainspotting" />
+    );
+    const buttons = ReactTestUtils.scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons[0].textContent).to.contain('Voted');
+  });
+
+  it('renders just the winner when there is one', () => {
+    const component = ReactTestUtils.renderIntoDocument(
+      <Voting winner="Trainspotting" />
+    );
+    const buttons = ReactTestUtils.scryRenderedDOMComponentsWithTag(component, 'button');
+    expect(buttons.length).to.equal(0);
+
+    const winner = ReactDOM.findDOMNode(component.refs.winner);
+    expect(winner).to.be.ok;
+    expect(winner.textContent).to.contain('Trainspotting');
+  });
 });
