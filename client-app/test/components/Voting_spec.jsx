cf87fa3c366723ce92ff44f0055441159440324f
Nur Rony <pro.nmrony@gmail.com>
2015-11-11 19:32:27 +0600

    added voting component tests

---
+++ client-app/test/components/Voting_spec.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactTestUtils from 'react-addons-test-utils';
+import Voting from '../../src/components/Voting';
+import {expect} from 'chai';
+
+
+describe('Voting', () => {
+  it('renders a pair of buttons', () => {
+    const component = ReactTestUtils.renderIntoDocument(
+      <Voting pair={['Trainspotting', '28 Days Later']}/>
+    );
+    const buttons = ReactTestUtils.scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons.length).to.equal(2);
+    expect(buttons[0].textContent).to.equal('Trainspotting');
+    expect(buttons[1].textContent).to.equal('28 Days Later');
+  });
+
+  it('invokes callback when a button is clicked', () => {
+    let votedWith;
+    const vote = (entry) => votedWith = entry;
+
+    const component = ReactTestUtils.renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 Days Later"]}
+              vote={vote}/>
+    );
+    const buttons = ReactTestUtils.scryRenderedDOMComponentsWithTag(component, 'button');
+    ReactTestUtils.Simulate.click(buttons[0]);
+    expect(votedWith).to.equal('Trainspotting');
+  });
+
+});
