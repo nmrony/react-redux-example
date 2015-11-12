8756bc7a1342eb503420375c5f3671307245bb69
Nur Rony <pro.nmrony@gmail.com>
2015-11-12 10:23:45 +0600

    next test case added for result component

--- client-app/src/index.jsx
+++ client-app/src/index.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import Router, {Route} from 'react-router';
 import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Result';
 
 const routes = <Route component={App}>
   <Route path="/" component={Voting} />
+  <Route path="/results" component={Results} />
 </Route>;
 
 ReactDOM.render(
